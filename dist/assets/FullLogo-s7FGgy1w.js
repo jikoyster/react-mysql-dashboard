363982adfe8cee11eec90f64d67d3034e66c9e8f
Jikoyster <yu.jake@gmail.com>
2025-10-15 10:11:05 +0800

    first commit

---
+++ dist/assets/FullLogo-s7FGgy1w.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-CaMpA6-9.js";const t="/assets/logo-Dq6uoD-m.svg",r=()=>o.jsx(s,{to:"/",children:o.jsx("img",{src:t,alt:"logo",className:"block"})});export{r as F};
---
+++ src/views/pages/Test.tsx
@@ -0,0 +1,16 @@
+
+
+const Test = () => {
+  return (
+    <>
+      <div>
+        <h5>Test Page</h5>
+        <div>
+          test page 
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Test;
